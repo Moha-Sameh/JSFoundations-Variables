611f0085c0109a4726731aa0c55b419428eb34cc
@Moha-Sameh <ms.slama01@hotmail.com>
2021-02-01 18:22:26 +0200

    Update

--- story.js
+++ story.js
@@ -1,8 +1,6 @@
 let just = "Mohamed";
 let me = 25;
 let be = "Unboxing"
-
-//console.log('${Mr ./Mrs.} ${just} ${ "in 33 years " + "you will be" } ');
 console.log("Mr ./Mrs.", just, "In 33 years, you will be"
-, me + 33, "years old. By then you'll have decades of HOBBY practice!"
+, me + 33, "years old. By then you'll have decades of", be, "practice!"
 );
